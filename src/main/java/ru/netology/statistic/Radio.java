0cb8e1575f71d3c4856f165a1d582ba9e4afa2dd
AleksandrEpishkin <aepishkin@yandex.ru>
2022-06-05 11:36:39 +0300

    modified constructors

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -2,20 +2,19 @@ package ru.netology.statistic;
 
 public class Radio {
 
+    private int quantityRadioStation;
     private int currentRadioStation;
-    private int quantityRadioStation = 10;
     private int maxVolume;
     private int currentVolume;
 
 
     public Radio() {
+        this.quantityRadioStation = 10;
+        this.maxVolume = 100;
     }
 
-    public Radio(int quantityRadioStation, int maxVolume) {
-        this.currentRadioStation = currentRadioStation;
+    public Radio(int quantityRadioStation) {
         this.quantityRadioStation = quantityRadioStation;
-        this.currentVolume = currentVolume;
-        this.maxVolume = maxVolume;
     }
 
 
@@ -57,11 +56,6 @@ public class Radio {
 
     public int getPrevStation() {
         return currentRadioStation;
-    }
-
-    public Radio(int maxVolume) {
-        this.currentVolume = currentVolume;
-        this.maxVolume = maxVolume;
     }
 
     public void setCurrentVolume(int currentVolume) {
